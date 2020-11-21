a158d33f9086621ad8fb989b0b89e13669f433d5
unknown <gian15249@gmail.com>
2020-11-21 11:53:38 -0400

    Create events e flash msgs

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Event;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
@@ -13,5 +14,18 @@ class EventController extends Controller
 
     public function create(){
         return view('events.create');
+    }
+
+    public function store(Request $request){
+
+        $event = new Event;
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        $event->save();
+
+        return redirect("/")->with('msg', 'Evento criado com sucesso!');
     }
 }
