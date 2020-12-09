8adba5673e5e71bb271e9451e4c0b0bb701e6777
unknown <gian15249@gmail.com>
2020-12-09 18:42:20 -0400

    Itens de infraestrutura do evento

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -30,6 +30,24 @@
                         <label for="title">Descrição:</label> 
                         <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
                   </div>
+                  <div class="form-group">
+                        <label for="title">Adicione itens de infraestrutura:</label> 
+                        <div class="form-group">
+                              <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+                        </div>
+                        <div class="form-group">
+                              <input type="checkbox" name="items[]" value="Palco"> Palco
+                        </div>
+                        <div class="form-group">
+                              <input type="checkbox" name="items[]" ivalued="Equipamento de Som"> Equipamento de Som
+                        </div>
+                        <div class="form-group">
+                              <input type="checkbox" name="items[]" value="Comida"> Comida
+                        </div>
+                        <div class="form-group">
+                              <input type="checkbox" name="items[]" value="Bebida"> Bebida
+                        </div>
+                  </div>
                   <input type="submit" class="btn btn-primary" value="Criar evento">
               </form> 
       </div>
