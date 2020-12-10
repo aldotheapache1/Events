49f4a709b821ab9413afa29dbd73e84a3791a6c9
unknown <gian15249@gmail.com>
2020-12-10 17:42:57 -0400

    One to Many usuario e seus eventos

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -55,8 +56,9 @@ class EventController extends Controller
 
     public function show($id) {
         $event = Event::findOrFail($id);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 
 }
