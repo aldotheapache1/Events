d55e033bed53efbe9cb0b8d6f8cc6a85caabc7c1
Gian Lucas <gian15249@gmail.com>
2020-12-21 18:38:20 -0300

    Update events

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -25,7 +25,7 @@
                 <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                 <td>0</td>
                 <td>
-                    <a class="btn btn-info edit-btn" href="#">
+                    <a class="btn btn-info edit-btn" href="events/edit/{{ $event->id }}">
                         <ion-icon name="create-outline"></ion-icon> Editar
                     </a>
                     <form action="/events/{{ $event->id }}" method="POST">
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,10 @@ Route::get('/',  [EventController::class, 'index']);
 Route::get('/events/create',  [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}',  [EventController::class, 'show']);
 Route::get('/dashboard',  [EventController::class, 'dashboard'])->middleware('auth');
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+
 Route::post('/events', [EventController::class, 'store']);
-Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
