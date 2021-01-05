fe385119af559444dffd083010a74e2912e9d136
Gian Lucas <gian15249@gmail.com>
2021-01-05 09:50:31 -0300

    Exibir eventos do usuario

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -44,5 +44,38 @@
     <p>Você ainda não possui eventos, <a href="/events/create">Criar eventos</a></p>
     @endif
 </div>
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Eventos que estou participando</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-events-container">
+    @if(count($eventsAsParticipant) > 0)
+        <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Participantes</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($eventsAsParticipant as $event)
+            <tr>
+                <td scope="row">{{ $loop->index + 1 }}</td>
+                <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                <td> {{ count($event->users) }}</td>
+                <td>
+                    <a class="btn btn-danger" href="#">
+                        Sair do evento
+                    </a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @else
+        <p>Você ainda não participa de nenhum evento, <a href="/">veja os eventos marcados.</a></p>
+    @endif
+</div>
 
 @endsection
